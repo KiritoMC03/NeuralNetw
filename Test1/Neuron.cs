12d5738e9ab31515e2777b195ed5c502d1a41d20
Kirill Dudnik <prog.me.2017@gmail.com>
2020-10-11 17:39:29 +0600

    Create.

---
+++ Test1/Neuron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test1
+{
+    public class Neuron
+    {
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            Type = type;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public List<double> Weights { get; }
+        public NeuronType Type { get; }
+        public double Output { get; private set; }
+        
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            return 0.0;
+        }
+    }
+}
